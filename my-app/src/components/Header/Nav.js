de4b15cf645cab3b8d1402bb64989c87413e7b4a
Russell Telen <rtelen@my.bcit.ca>
2020-11-09 15:50:13 -0800

    add Nav html elements

--- my-app/src/components/Header/Nav.js
+++ my-app/src/components/Header/Nav.js
@@ -1,9 +1,56 @@
 import React, { Component } from "react";
+import { BiCameraMovie } from "react-icons/bi";
 
 class Nav extends Component {
   state = {};
   render() {
-    return <h1>Nav Works</h1>;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="index.html">
+          <BiCameraMovie /> Movie App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="index.html">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">
+                About
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Not Functioning Yet"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    );
   }
 }
 

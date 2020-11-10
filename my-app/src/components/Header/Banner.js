c75dd51106b1e000e864f00f9fb32360d71e88a9
Russell Telen <rtelen@my.bcit.ca>
2020-11-09 16:33:08 -0800

    add banner section

--- my-app/src/components/Header/Banner.js
+++ my-app/src/components/Header/Banner.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Banner extends Component {
   render() {
-    return <h1>Banner Works</h1>;
+    return (
+      <div className="jumbotron">
+        <h1 className="display-4">Greatest And Latest Movies 2020</h1>
+        <p className="lead">
+          Ad eiusmod ut deserunt irure sint. Tempor velit cupidatat irure anim
+          irure excepteur. Do laborum tempor consequat do sint aliquip ad fugiat
+          eiusmod quis. Do ullamco id quis in ea aliqua Lorem nisi ad. Nostrud
+          quis ut commodo aute officia ullamco ad sunt in. Magna sit elit amet
+          sit laborum elit est culpa quis ex exercitation.
+        </p>
+      </div>
+    );
   }
 }
 

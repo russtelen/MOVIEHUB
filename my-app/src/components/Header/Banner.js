f956c1b3d3ccacf7609c0d72c7ef2df2f23e21c6
Russell Telen <rtelen@my.bcit.ca>
2020-11-13 15:48:33 -0800

    update banner

--- my-app/src/components/Header/Banner.js
+++ my-app/src/components/Header/Banner.js
@@ -3,17 +3,11 @@ import React, { Component } from "react";
 class Banner extends Component {
   render() {
     return (
-      <div className="jumbotron d-flex">
+      <div className="jumbotron">
         <div className="align-self-center">
-          <h1 className="display-4 text-center">
-            Greatest And Latest Movies 2020
-          </h1>
+          <h1 className="text-center">Movie App</h1>
           <p className="lead text-center">
-            Ad eiusmod ut deserunt irure sint. Tempor velit cupidatat irure anim
-            irure excepteur. Do laborum tempor consequat do sint aliquip ad
-            fugiat eiusmod quis. Do ullamco id quis in ea aliqua Lorem nisi ad.
-            Nostrud quis ut commodo aute officia ullamco ad sunt in. Magna sit
-            elit amet sit laborum elit est culpa quis ex exercitation.
+            Millions of movies, TV shows and people to discover. Explore now.
           </p>
         </div>
       </div>

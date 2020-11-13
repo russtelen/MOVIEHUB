bb2dcfeae97faa1251cf92f2f23d7a478e6e2c43
Russell Telen <rtelen@my.bcit.ca>
2020-11-13 15:29:03 -0800

    style movie cards

--- my-app/src/components/Main/MainMovieItem.js
+++ my-app/src/components/Main/MainMovieItem.js
@@ -5,19 +5,22 @@ class MainMovieItem extends Component {
   render() {
     return (
       <div className="movie-item col-md-6 col-lg-4 py-2">
-        <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center pb-2">
           <img
             src={this.props.img}
             width="300px"
             height="400px"
             alt="Image is not loaded"
             border="10"
+            className="movie-image"
           />
         </div>
 
         <div className="text-center">
-          <p>{this.props.title}</p>
-          <p>{this.props.release}</p>
+          <p className="movie-title">{this.props.title}</p>
+          <p className="movie-release-date">
+            Release Date: {this.props.release}
+          </p>
           <button className="btn btn-secondary">More Info</button>
         </div>
       </div>
